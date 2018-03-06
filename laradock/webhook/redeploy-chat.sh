36cb5df10bc49ba5741200a31863b5204c2c2ca8
Dragomeat <dragomeat@dragomeat.com>
2018-03-06 23:11:31 +0300

    Small changes and fixes

--- laradock/webhook/redeploy-chat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls -al /var
-
-PROJECT_PATH="/var/www"
-COMMIT_ID=$1
-PUSHER_NAME=$2
-
-su -c "cd $PROJECT_PATH; artisan down; git pull; composer install; artisan up" -s /bin/bash laradock
